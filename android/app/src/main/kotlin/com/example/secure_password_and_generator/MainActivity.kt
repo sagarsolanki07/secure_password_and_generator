a8bca11a19faa4a59fa072eb3056d627b052c502
sagarsolanki07 <108773917+sagarsolanki07@users.noreply.github.com>
2023-07-27 19:28:08 +0530

    n1

--- android/app/src/main/kotlin/com/example/secure_password_and_generator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/secure_password_and_generator/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.secure_password_and_generator
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    // ...
 }
